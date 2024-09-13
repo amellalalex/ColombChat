42ccd091b24f1a598b57cbd9aee8388686fe6246
amellalalex <github@alexamellal.com>
2024-09-13 14:41:14 -0300

    Bidirectional fixed but with deadlock

--- ColombChat/chat.py
+++ ColombChat/chat.py
@@ -2,6 +2,7 @@ import socket
 import time
 import logging
 import threading
+import sys
 
 from peer import Peer
 from settings import *
@@ -39,6 +40,48 @@ def monitor_peer_for_incoming_msg(peer):
     # end while True
     peers.remove(peer)
 
+def process_msg_as_cmd(msg):
+    if len(msg) > 1 and msg[0] == '/':
+        run_cmd(msg)
+        return True 
+    else:
+        return False
+
+def run_cmd(msg):
+    tokens = msg.split(' ')
+    
+    if tokens[0] == '/connect' and len(tokens) >= 3:
+        ip = tokens[1]
+        port = int(tokens[2])
+
+        logging.info('Connecting to peer '+str((ip, port))+'.')
+        s = socket.socket()
+        s.connect((ip, port)) # fmt = /connect <ip> <port>
+
+        peer = Peer((s, (ip, port)))
+        peers.append(peer)
+        peers[-1].handle = threading.Thread(target=monitor_peer_for_incoming_msg, args=(peers[-1],))
+        peers[-1].handle.start()
+    # endif tokens[0] == '/connect' and len(tokens) >= 3
+    elif tokens[0] == '/connect' and len(tokens) == 2:
+        split_addr = tokens[1].split(':')
+        ip = split_addr[0]
+        port = int(split_addr[1])
+
+        logging.info('Connecting to peer '+str(tokens[1])+'.')
+        s = socket.socket()
+        s.connect((ip, port))
+
+        peer = Peer((s, (ip, port)))
+        peers.append(peer)
+        peers[-1].handle = threading.Thread(target=monitor_peer_for_incoming_msg, args=(peers[-1],))
+        peers[-1].handle.start()
+        
+    # endif tokens[0] == '/connect' and len(tokens) == 2
+    elif tokens[0] == '/close':
+        for peer in peers:
+            peer.close()
+
 if __name__ == '__main__':
     # Setup logging
     format = "%(asctime)s: %(message)s"
@@ -48,13 +91,18 @@ if __name__ == '__main__':
         datefmt="%H:%M:%S"
     )
     
+    # Override default port is command line arg present 
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    
     # Start accepting incoming connections on thread 
     accept_incoming_thread = threading.Thread(target=accept_incoming)
     accept_incoming_thread.start()
 
     while True:
         msg = input('>> ')
-        for peer in peers:
-            peer.send(msg)
+        if not process_msg_as_cmd(msg):
+            for peer in peers:
+                peer.send(msg)
 
     accept_incoming_thread.join()
--- ColombChat/peer.py
+++ ColombChat/peer.py
@@ -1,18 +1,29 @@
 from settings import *
 import socket
+import threading
 
 class Peer:
     def __init__(self, conn_addr_pair=(None, '0.0.0.0')):
         self.conn = conn_addr_pair[0]
         self.addr = conn_addr_pair[1]
         self.handle = None
+        self.lock = threading.Lock()
         
     def get(self):
+        self.lock.acquire()
         msg = self.conn.recv(msglen)
+        self.lock.release()
         if not msg:
             return None
         else:
             return msg
     
     def send(self, msg):
+        self.lock.acquire()
         self.conn.send(bytes(msg, 'utf-8'))
+        self.lock.release()
+        
+    def close(self):
+        self.lock.acquire()
+        self.conn.close()
+        self.lock.release()
